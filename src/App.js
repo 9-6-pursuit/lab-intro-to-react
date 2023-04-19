95595f56a8cb28e3cf39700ebd366b025682cc42
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-04-18 22:37:34 -0400

    React-Day-One

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import NavBar from './Components/NavBar.js'
+import Posts from './Components/Posts.js'
+import Contacts from './Components/Contacts.js'
+import UserProfile from './Components/UserProfile.js'
 import "./index.css";
 
 function App () {
-  return <p>Hello, world!</p>;
+  return <div>
+    <NavBar></NavBar>
+    <UserProfile></UserProfile>
+    <Posts></Posts>
+    <Contacts></Contacts>
+  </div>;
 };
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,5 @@
+function NavBar () {
+    return <p>This is the Nav Bar!</p>;
+};
+
+export default NavBar;
