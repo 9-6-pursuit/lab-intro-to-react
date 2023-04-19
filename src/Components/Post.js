6e5a5de8cdbd5fa6eac6604a37e601a440a75c72
amshepard <antoineshepard@pursuit.org>
2023-04-18 23:08:59 -0400

    all tests passing.

---
+++ src/Components/Post.js
@@ -0,0 +1,13 @@
+function Post() {
+
+    return (
+        <div class="post">
+            <p><strong>Title</strong></p>
+            <p>Pitchfork kombucha pork belly paleo ascot, palo santo 3 wolf moon poutine hot chicken.
+
+</p>
+        </div>
+    )
+}
+
+export default Post;
