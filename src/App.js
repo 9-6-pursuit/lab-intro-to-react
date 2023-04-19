5b6abe19fa13ce40f38ecde812e56e7ac3b64784
ayd823 <ayd823@gmail.com>
2023-04-19 15:26:56 -0400

    Intro to React (No CSS)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import UserProfile from "./Components/UserProfile";
+import Posts from "./Components/Posts";
+import Contacts from "./Components/Contacts";
 
-function App () {
-  return <p>Hello, world!</p>;
-};
+function App() {
+  return (
+    <>
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+    </>
+  );
+}
 
 export default App;
