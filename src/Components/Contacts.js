58f4572aa03a3c6fa96cb20a078635ccdb14e733
RadiantRay22 <rayrussell@pursuit.org>
2023-04-20 12:47:00 -0400

    Ray is done

---
+++ src/Components/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContactUserCard from './ContactUserCard'
+
+function Contacts() {
+  return (
+    <div>
+        <p>Contacts</p>
+       <ContactUserCard/> 
+       <ContactUserCard/> 
+       <ContactUserCard/> 
+       <ContactUserCard/> 
+    </div>
+  )
+}
+
+export default Contacts
