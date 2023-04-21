e23bd91f5e0da37eaaf33cd22b0d7e468b0c37cd
Lakisha J <lakishajohnson@pursuit.org>
2023-04-21 13:40:45 -0400

    React Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import Contacts from "./Components/Contacts";
+import UserProfile from "./Components/UserProfile";
+import Posts from "./Components/Posts";
 
-function App () {
-  return <p>Hello, world!</p>;
-};
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <UserProfile />
+      <Posts />
+      <Contacts />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+function UserProfile () {
+    return (
+        <div className="UserProfile" >
+            <div>
+                <img src="./profile.png" alt="User"/> </div>
+            <h2>User</h2>
+            <h4>React Creator</h4>
+            <p>Something muddy</p>
+        </div>
+    )
+}
+            
+
+export default UserProfile
