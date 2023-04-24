272b097617fbe19b078a51e02dfac4796e18d89f
LELIAH <LELIAH791@GMAIL.COM>
2023-04-24 10:08:03 -0400

    intro-to-react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import UserProfile from "./Components/UserProfile";
+import Contacts from "./Components/Contacts";
+import Posts from "./Components/Posts";
 
 function App () {
-  return <p>Hello, world!</p>;
+  return (
+    <div className="app">
+      <NavBar />
+      <main>
+        <UserProfile />
+        <Contacts />
+        <Posts />
+      </main>
+    </div>
+
+    )
 };
 
 export default App;
---
+++ src/Components/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post'
+
+function Posts() {
+  return (
+    <div className='posts'>
+        <h1>Posts</h1>
+        <Post />
+        <Post />
+        <Post />
+    </div>
+  )
+}
+
+export default Posts
