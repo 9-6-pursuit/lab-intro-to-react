9bd23e05b2aa32219405b8dc7cf18f07d4dfe238
AlejandraRV91 <alejandraramirez@pursuit.org>
2023-04-25 00:29:24 -0500

    lab react intro labs DONE

---
+++ src/Components/Contacts.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from "react";
+import "../index.css";
+
+function Contacts({children}) {
+	return <div className="contact">
+        {children}
+    </div>;
+}
+
+export default Contacts;
---
+++ src/Components/Posts.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from "react";
+import "../index.css";
+
+function Posts({children}) {
+	return <div className="posts">
+        {children}
+    </div>;
+}
+
+export default Posts;
