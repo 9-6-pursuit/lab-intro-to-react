74b8b7c90c1382a731c22fef670f6d1666140871
salvagebar <salvagebar@gmail.com>
2023-05-29 11:41:24 -0400

    Navbar.js and UserProfile.js written

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import UserProfile from "./Components/UserProfile";
 
 function App () {
   return <p>Hello, world!</p>;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../index.css";
+
+function NavBar() {
+  return <header className="navbar">Navigation bar goes here.</header>;
+}
+
+export default NavBar;
