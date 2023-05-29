2317ada1632975ec857f03797b7cb48cffc13a8e
salvagebar <salvagebar@gmail.com>
2023-05-29 11:59:32 -0400

    Contacts and User cards and Posts mocked up

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./index.css";
 import NavBar from "./Components/NavBar";
 import UserProfile from "./Components/UserProfile";
+import Posts from "./Components/Posts";
 
 function App () {
-  return <p>Hello, world!</p>;
+  return <div>
+    <NavBar></NavBar>
+    <UserProfile></UserProfile>
+    <Posts></Posts>
+  </div>;
 };
 
 export default App;
---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,5 @@
+function ContactUserCard() {
+  return <p>Here's a user's info.</p>;
+}
+
+export default ContactUserCard;
