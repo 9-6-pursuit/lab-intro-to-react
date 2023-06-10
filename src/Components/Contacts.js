b11dff3fc82b2aa3e9306862d84303a549218b71
winterbelle <blancaaltamirano@pursuit.org>
2023-06-10 15:50:41 -0400

    lab complete

---
+++ src/Components/Contacts.js
@@ -0,0 +1,14 @@
+import ContactUserCard from "./ContactUserCard";
+
+function Contacts() {
+    return (
+        <div>
+            <h3>My Contacts</h3>
+            <ContactUserCard />
+            <ContactUserCard />
+            <ContactUserCard />
+        </div>
+    )
+}
+
+export default Contacts;
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,6 @@
+function UserProfile() {
+    return <p>This is the user's profile section.</p>
+}
+
+export default UserProfile
+
